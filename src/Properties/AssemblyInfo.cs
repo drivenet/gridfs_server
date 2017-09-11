758971fdc6ffcdf093dd8314d58080675ea7129a
Aristarkh Zagorodnikov <xm@drive.net>
2017-09-11 11:53:54 +0300

    Cleanup analysis/style/project

--- src/Properties/AssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-﻿[assembly: System.Reflection.AssemblyTitle("gridfs_server")]
-[assembly: System.Reflection.AssemblyProduct("GridFS server")]
-[assembly: System.Reflection.AssemblyCopyright("Copyright © DRIVE.NET, Inc. 2017")]
-[assembly: System.Reflection.AssemblyVersion("1.2.0")]
-
-// [assembly: System.Runtime.CompilerServices.InternalsVisibleTo("gridfs_server.Tests")]
-// [assembly: System.Runtime.CompilerServices.InternalsVisibleTo("DynamicProxyGenAssembly2")]
