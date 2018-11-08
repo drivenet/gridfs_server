39b7f538773d2384f646c9df3ac8a93e571fa070
Aristarkh Zagorodnikov <xm@drive.net>
2018-11-08 14:31:45 +0300

    Cleanup

--- src/Composition/Program.cs
+++ src/Composition/Program.cs
@@ -5,7 +5,6 @@ using Microsoft.AspNetCore.Server.Kestrel.Core;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Logging.Console;
 
 namespace GridFSServer.Composition
 {
