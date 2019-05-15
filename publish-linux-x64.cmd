d0f25b64f22dda20edca10adbee7c0f1b527bf3b
Aristarkh Zagorodnikov <xm@drive.net>
2019-05-15 13:34:34 +0300

    Fix x64 platform restriction

--- publish-linux-x64.cmd
+++ publish-linux-x64.cmd
@@ -3,7 +3,7 @@ rmdir /s /q src\bin\Release\netcoreapp2.2\linux-x64\publish
 dotnet publish -c Release -r linux-x64 --self-contained false
 rmdir /s /q packages\gridfs_server-linux-x64
 mkdir packages\gridfs_server-linux-x64
-xcopy src\bin\Release\netcoreapp2.2\linux-x64\publish\*.dll packages\gridfs_server-linux-x64
-xcopy src\bin\Release\netcoreapp2.2\linux-x64\publish\*.pdb packages\gridfs_server-linux-x64
-xcopy src\bin\Release\netcoreapp2.2\linux-x64\publish\*.so packages\gridfs_server-linux-x64
-xcopy src\bin\Release\netcoreapp2.2\linux-x64\publish\*.runtimeconfig.json packages\gridfs_server-linux-x64
+xcopy src\bin\x64\Release\netcoreapp2.2\linux-x64\publish\*.dll packages\gridfs_server-linux-x64
+xcopy src\bin\x64\Release\netcoreapp2.2\linux-x64\publish\*.pdb packages\gridfs_server-linux-x64
+xcopy src\bin\x64\Release\netcoreapp2.2\linux-x64\publish\*.so packages\gridfs_server-linux-x64
+xcopy src\bin\x64\Release\netcoreapp2.2\linux-x64\publish\*.runtimeconfig.json packages\gridfs_server-linux-x64
