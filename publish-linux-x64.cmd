d7576072a39cd33fb99ce6b09be879fb9c3ceae6
Aristarkh Zagorodnikov <xm@drive.net>
2019-05-15 13:19:54 +0300

    Use FDE publish

--- publish-linux-x64.cmd
+++ publish-linux-x64.cmd
@@ -1,9 +1,9 @@
 @echo off
-rmdir /s /q src\bin\Release\netcoreapp2.2\publish
-dotnet publish -c Release -f netcoreapp2.2
+rmdir /s /q src\bin\Release\netcoreapp2.2\linux-x64\publish
+dotnet publish -c Release -r linux-x64 --self-contained false
 rmdir /s /q packages\gridfs_server-linux-x64
 mkdir packages\gridfs_server-linux-x64
-xcopy src\bin\Release\netcoreapp2.2\publish\*.dll packages\gridfs_server-linux-x64
-xcopy src\bin\Release\netcoreapp2.2\publish\*.pdb packages\gridfs_server-linux-x64
-xcopy src\bin\Release\netcoreapp2.2\publish\*.runtimeconfig.json packages\gridfs_server-linux-x64
-xcopy src\bin\Release\netcoreapp2.2\publish\runtimes\linux-x64\native\*.so packages\gridfs_server-linux-x64
+xcopy src\bin\Release\netcoreapp2.2\linux-x64\publish\*.dll packages\gridfs_server-linux-x64
+xcopy src\bin\Release\netcoreapp2.2\linux-x64\publish\*.pdb packages\gridfs_server-linux-x64
+xcopy src\bin\Release\netcoreapp2.2\linux-x64\publish\*.so packages\gridfs_server-linux-x64
+xcopy src\bin\Release\netcoreapp2.2\linux-x64\publish\*.runtimeconfig.json packages\gridfs_server-linux-x64
