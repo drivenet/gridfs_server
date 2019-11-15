9d1e99c89f0feb8d3695bf08a1b2c8de0eb15ac9
Aristarkh Zagorodnikov <xm@drive.net>
2019-11-15 13:17:20 +0300

    Bump version and fix publish

--- publish-linux-x64.cmd
+++ publish-linux-x64.cmd
@@ -1,6 +1,6 @@
 @echo off
 rmdir /s /q packages\linux-x64\gridfs_server
 mkdir packages\linux-x64\gridfs_server
-dotnet publish src --output ..\packages\linux-x64\gridfs_server -c Release -r linux-x64 --self-contained false
+dotnet publish src --output packages\linux-x64\gridfs_server -c Release -r linux-x64 --self-contained false
 rmdir /s /q packages\linux-x64\gridfs_server\refs
 del packages\linux-x64\gridfs_server\gridfs_server packages\linux-x64\gridfs_server\web.config packages\linux-x64\gridfs_server\*.deps.json
