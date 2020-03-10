b01967553b36b49fa9a1750cdb2d3273b28a83bb
Aristarkh Zagorodnikov <xm@drive.net>
2020-03-10 12:40:57 +0300

    Refresh dependencies

--- publish-linux-x64.cmd
+++ publish-linux-x64.cmd
@@ -2,4 +2,6 @@
 rmdir /s /q packages\linux-x64\gridfs_server
 mkdir packages\linux-x64\gridfs_server
 dotnet publish src --output packages\linux-x64\gridfs_server -c Release -r linux-x64 --self-contained false
-del packages\linux-x64\gridfs_server\gridfs_server packages\linux-x64\gridfs_server\web.config packages\linux-x64\gridfs_server\*.deps.json
+move packages\linux-x64\gridfs_server\Microsoft.AspNetCore.Server.Kestrel.Transport.Libuv.dll "%TEMP%"
+del packages\linux-x64\gridfs_server\gridfs_server packages\linux-x64\gridfs_server\web.config packages\linux-x64\gridfs_server\*.deps.json packages\linux-x64\gridfs_server\*settings.json packages\linux-x64\gridfs_server\Microsoft.*.dll
+move "%TEMP%\Microsoft.AspNetCore.Server.Kestrel.Transport.Libuv.dll" packages\linux-x64\gridfs_server
