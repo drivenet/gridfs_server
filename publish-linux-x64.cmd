921cefd2bedfb659bba058ce9c446ab73bd8b6e1
Aristarkh Zagorodnikov <xm@drive.net>
2021-03-10 14:50:48 +0300

    Improve publish and bump version

--- publish-linux-x64.cmd
+++ publish-linux-x64.cmd
@@ -4,6 +4,6 @@ mkdir packages\linux-x64\gridfs_server
 dotnet publish src --output packages\linux-x64\gridfs_server -c Integration -r linux-x64 --self-contained false
 move packages\linux-x64\gridfs_server\Microsoft.Extensions.Hosting.Systemd.dll "%TEMP%"
 move packages\linux-x64\gridfs_server\Microsoft.AspNetCore.Buffering.dll "%TEMP%"
-del packages\linux-x64\gridfs_server\web.config packages\linux-x64\gridfs_server\*.deps.json packages\linux-x64\gridfs_server\*settings.json packages\linux-x64\gridfs_server\Microsoft.*.dll
+del packages\linux-x64\gridfs_server\web.config packages\linux-x64\gridfs_server\*.deps.json packages\linux-x64\gridfs_server\Microsoft.*.dll
 move "%TEMP%\Microsoft.Extensions.Hosting.Systemd.dll" packages\linux-x64\gridfs_server
 move "%TEMP%\Microsoft.AspNetCore.Buffering.dll" packages\linux-x64\gridfs_server
