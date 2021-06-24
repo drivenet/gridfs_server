4b493fe8aec1fd838c6d0cff638d74be8ce06570
Aristarkh Zagorodnikov <xm@drive.net>
2021-06-24 19:12:07 +0300

    Update build scripts

--- publish-linux-x64.cmd
+++ publish-linux-x64.cmd
@@ -1,7 +1,7 @@
 @echo off
 rmdir /s /q packages\linux-x64\gridfs_server
 mkdir packages\linux-x64\gridfs_server
-dotnet publish src --output packages\linux-x64\gridfs_server -c Integration -r linux-x64 --self-contained false
+dotnet publish src --force --output packages\linux-x64\gridfs_server -c Integration -r linux-x64 --self-contained false
 move packages\linux-x64\gridfs_server\Microsoft.Extensions.Hosting.Systemd.dll "%TEMP%"
 move packages\linux-x64\gridfs_server\Microsoft.AspNetCore.Buffering.dll "%TEMP%"
 del packages\linux-x64\gridfs_server\web.config packages\linux-x64\gridfs_server\*.deps.json packages\linux-x64\gridfs_server\Microsoft.*.dll
--- publish-linux-x64.sh
+++ publish-linux-x64.sh
@@ -3,7 +3,7 @@
 rm -rdf packages/linux-x64/gridfs_server
 mkdir -p packages/linux-x64/gridfs_server
 
-dotnet publish src --output packages/linux-x64/gridfs_server -c Integration -r linux-x64 --self-contained false
+dotnet publish src --force --output packages/linux-x64/gridfs_server -c Integration -r linux-x64 --self-contained false
 
 mv packages/linux-x64/gridfs_server/Microsoft.Extensions.Hosting.Systemd.dll /tmp
 mv packages/linux-x64/gridfs_server/Microsoft.AspNetCore.Buffering.dll /tmp
