f567d3e43c6ba5c4546b0eb1770cf1b0b096eebd
Alexander Tischenko <tsm@fiberside.ru>
2021-06-24 14:35:23 +0300

    Linux build script

---
+++ publish-linux-x64.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+
+rm -rdf packages/linux-x64/gridfs_server
+mkdir -p packages/linux-x64/gridfs_server
+
+dotnet publish src --output packages/linux-x64/gridfs_server -c Integration -r linux-x64 --self-contained false
+
+mv packages/linux-x64/gridfs_server/Microsoft.Extensions.Hosting.Systemd.dll /tmp
+mv packages/linux-x64/gridfs_server/Microsoft.AspNetCore.Buffering.dll /tmp
+rm packages/linux-x64/gridfs_server/web.config packages/linux-x64/gridfs_server/*.deps.json packages/linux-x64/gridfs_server/Microsoft.*.dll
+mv "/tmp/Microsoft.Extensions.Hosting.Systemd.dll" packages/linux-x64/gridfs_server
+mv "/tmp/Microsoft.AspNetCore.Buffering.dll" packages/linux-x64/gridfs_server
+
