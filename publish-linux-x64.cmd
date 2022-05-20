9b8a199ca6cbb87e3990f8341b68c9bdd750eb50
Aristarkh Zagorodnikov <xm@x-infinity.com>
2022-05-20 13:16:22 +0300

    Update publish script

--- publish-linux-x64.cmd
+++ publish-linux-x64.cmd
@@ -1,9 +1,5 @@
 @echo off
 rmdir /s /q packages\linux-x64\gridfs_server
 mkdir packages\linux-x64\gridfs_server
-dotnet publish src --force --output packages\linux-x64\gridfs_server -c Integration -r linux-x64 --self-contained false
-move packages\linux-x64\gridfs_server\Microsoft.Extensions.Hosting.Systemd.dll "%TEMP%"
-move packages\linux-x64\gridfs_server\Microsoft.IO.RecyclableMemoryStream.dll "%TEMP%"
-del packages\linux-x64\gridfs_server\web.config packages\linux-x64\gridfs_server\*.deps.json packages\linux-x64\gridfs_server\Microsoft.*.dll
-move "%TEMP%\Microsoft.Extensions.Hosting.Systemd.dll" packages\linux-x64\gridfs_server
-move "%TEMP%\Microsoft.IO.RecyclableMemoryStream.dll" packages\linux-x64\gridfs_server
+dotnet publish src --force --output packages\linux-x64\gridfs_server -c Integration -r linux-x64 --no-self-contained
+del packages\linux-x64\gridfs_server\*.deps.json
