4f776aee9a66231a8dc265be9fa5b3f952033d5a
Aristarkh Zagorodnikov <xm@x-infinity.com>
2022-06-22 18:12:50 +0300

    Update publish script

--- publish-linux-x64.sh
+++ publish-linux-x64.sh
@@ -2,12 +2,5 @@
 
 rm -rdf packages/linux-x64/gridfs_server
 mkdir -p packages/linux-x64/gridfs_server
-
-dotnet publish src --force --output packages/linux-x64/gridfs_server -c Integration -r linux-x64 --self-contained false
-
-mv packages/linux-x64/gridfs_server/Microsoft.Extensions.Hosting.Systemd.dll /tmp
-mv packages/linux-x64/gridfs_server/Microsoft.AspNetCore.Buffering.dll /tmp
-rm packages/linux-x64/gridfs_server/web.config packages/linux-x64/gridfs_server/*.deps.json packages/linux-x64/gridfs_server/Microsoft.*.dll
-mv "/tmp/Microsoft.Extensions.Hosting.Systemd.dll" packages/linux-x64/gridfs_server
-mv "/tmp/Microsoft.AspNetCore.Buffering.dll" packages/linux-x64/gridfs_server
-
+dotnet publish src --force --output packages/linux-x64/gridfs_server -c Integration -r linux-x64 --no-self-contained
+rm packages/linux-x64/gridfs_server/*.deps.json
