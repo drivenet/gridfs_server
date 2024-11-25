5d18a986d9dd0b7832680cf7e9c483d2a8051ae6
Aristarkh Zagorodnikov <xm@x-infinity.com>
2024-11-25 13:34:01 +0000

    build: remove unnecessary file

--- publish-linux-x64.cmd
+++ publish-linux-x64.cmd
@@ -2,4 +2,4 @@
 rmdir /s /q packages\linux-x64\gridfs_server
 mkdir packages\linux-x64\gridfs_server
 dotnet publish src --force --output packages\linux-x64\gridfs_server -c Integration -r linux-x64 --no-self-contained
-del packages\linux-x64\gridfs_server\*.deps.json
+del packages\linux-x64\gridfs_server\*.deps.json packages\linux-x64\gridfs_server\*.endpoints.json
